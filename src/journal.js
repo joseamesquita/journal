403ebb83d14fa139eeacbf8ef8dbfa00e14dfe72
erich richter, Jose Amesquita and Tiffany Nguyen <erich@>
2020-02-03 16:03:38 -0800

    1. count the total words, vowels, and consonants

--- src/journal.js
+++ src/journal.js
@@ -1,4 +1,40 @@
-export function journal(goal) {
-    var result = goal;
-    return result;
+export function Journal(title, sentence) {
+  this.title = title,
+  this.sentence = sentence,
+  this.count = 0;
+  
+}
+
+Journal.prototype.Count = function(sum) {
+  var countArray = sum.split(" ");
+  this.count = countArray.length;
+  return this.count;
+};
+
+Journal.prototype.Vowels = function(word){
+  var result = word.toLowerCase();
+  var totalV = "";
+  
+  console.log(result)
+  var vowels = ['a','e','i','o','u'];
+  for(var i = 0; i < result.length; i++){
+    if((vowels.includes(result[i]))){
+      totalV += result[i];
+    }
   }
+  return totalV.length;
+}
+
+Journal.prototype.Cons = function(word){
+  var result = word.toLowerCase();
+  
+  var totalC = "";
+  console.log(result)
+  var vowels = ['a','e','i','o','u', ' '];
+  for(var i = 0; i < result.length; i++){
+    if(!(vowels.includes(result[i]))){
+      totalC += result[i];
+  }
+}
+  return totalC.length;
+}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,29 @@
-import { journal } from './journal';
+import { Journal } from './journal'; 
+import './styles.css';
 
-$(document).ready(function() {
-  $('#journal-form').submit(function(event) {
+
+$(document).ready(function () {
+  $('#journal-form').submit(function (event) {
     event.preventDefault();
-    var goal = $('#goal').val();
-    console.log(goal)
-    var output = journal(goal);
-    console.log(output)
+    var title = $('#goal').val();
+    var sentence = $('#goal2').val();
+
+    var output = new Journal(title, sentence);
+    var titleLength = output.Count(output.title);
+    var sentenceLength = output.Count(output.sentence);
     
-    $('#solution').text(output);
+    var vowelTitle = output.Vowels(output.title);
+    var vowelSentence = output.Vowels(output.sentence);
+
+    var consTitle = output.Cons(output.title);
+    var consSentence = output.Cons(output.sentence);
+
+    $('#header').text(output.title + " " + titleLength);
+    $('#body').text(output.sentence + " " + sentenceLength);
+    $('#vowelTitle').text("vowels in the title: " + vowelTitle);
+    $('#vowelSentence').text("vowels in the sentence: " + vowelSentence);
+    $('#consTitle').text("cons in the title: " + consTitle);
+    $('#consSentence').text("cons in the sentence: " + consSentence);
+
   });
 });
